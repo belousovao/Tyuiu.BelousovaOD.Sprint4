5016ea184a8b7b3c646802bc8db91e9a33e9e1cf
Ангелина Молчанкина <molchankinaap@outlook.com>
2024-11-13 12:50:36 +0500

    Task6

---
+++ Tyuiu.BelousovaOD.Sprint4.Task6.V14.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.BelousovaOD.Sprint4.Task6.V14.Lib;
+namespace Tyuiu.BelousovaOD.Sprint4.Task6.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            string[] months = { "Январь", "Февраль ", "Март", "Апрель", "Май", "Июнь", "Июль" };
+            string[] res = ds.Calculate(months);
+            string[] wait = { "Январь", "Февраль ", "Март", "Апрель", "Май", "Июнь", "Июль" };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
