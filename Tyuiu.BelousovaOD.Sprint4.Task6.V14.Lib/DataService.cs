e67bbcf4f00bd78c1369c5e387945f76846152cf
Tyuiu.BelousovaOD@outlook.com <bbb_od@mail.ru>
2024-11-27 10:26:54 +0500

    Task6

---
+++ Tyuiu.BelousovaOD.Sprint4.Task6.V14.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.BelousovaOD.Sprint4.Task6.V14.Lib
+{
+    public class DataService : ISprint4Task6V14
+    {
+        public string[] Calculate(string[] array)
+        {
+            string[] months = Array.FindAll(array, month => month.Length > 3);
+            return months;
+
+        }
+    }
+}
+
---
+++ Tyuiu.BelousovaOD.Sprint4.Task6.V14.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.BelousovaOD.Sprint4.Task6.V14.Lib;
+namespace Tyuiu.BelousovaOD.Sprint4.Task6.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string[] months = { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль" };
+            string[] res = ds.Calculate(months);
+            string[] wait = { "Январь", "Февраль", "Март", "Апрель", "Июнь", "Июль" };
+
+            CollectionAssert.AreEqual(wait, res);
+
+        }
+    }
+}
+
+
+
+
