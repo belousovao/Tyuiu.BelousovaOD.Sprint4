5ff6a1d58aff93b3e20e3e79d6bf0db4357ddb02
Tyuiu.BelousovaOD@outlook.com <bbb_od@mail.ru>
2025-01-18 09:24:13 +0500

    Task7

---
+++ Tyuiu.BelousovaOD.Sprint4.Task7.V26.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BelousovaOD.Sprint4.Task7.V26.Lib;
+
+namespace Tyuiu.BelousovaOD.Sprint4.Task7.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int[] numsArray = { 2, 2, 2, 5, 5, 3, 4, 2, 5, 2, 5, 3, 4, 3, 5, 2 };
+            int res = ds.Calculate(numsArray);
+            int Wait = 84375;
+            Assert.AreEqual(Wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BelousovaOD.Sprint4.Task7.V26/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.BelousovaOD.Sprint4.Task7.V26.Lib;
+namespace Tyuiu.BelousovaOD.Sprint4.Task7.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* Спринт #4                                                            *");
+            Console.WriteLine("* Задание #7 Вариант #26                                               *");
+            Console.WriteLine("************************************************************************");
+
+            int n = 5;
+            int m = 3;         
+            int[,] mtrx = new int[n, m];
+            int index = 0;
+            string value = "351268459614723";
+
+            
+
+
+            Console.WriteLine("\nМассив: ");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write($"{value[index]}\t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                           *");
+            Console.WriteLine("************************************************************************");
+
+            int res = ds.Calculate(n, m, value);
+
+            Console.WriteLine($"Ответ = {res} ");
+            Console.ReadKey();
+
+        }
+    }
+}
